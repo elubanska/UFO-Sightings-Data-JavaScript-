c0a0e1aec19f9641aa6216dfab385918cd25d05c
Emilia <e.lubanska@gmail.com>
2021-01-13 19:20:31 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,8 @@
 // from data.js
 var tableData = data;
 // Select the button
+var tbody = d3.select("tbody");
+
 var button = d3.select("#button");
 
 // Select the form
@@ -9,6 +11,14 @@ var form = d3.select("#form");
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
+
+data.forEach((UFOReport) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOReport).forEach(([value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
 
 function runEnter() {
 
@@ -24,32 +34,9 @@ function runEnter() {
     console.log(inputValue);
     console.log(tableData);
   
-    /*var filteredData = people.filter(person => person.bloodType === inputValue);
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
   
     console.log(filteredData);
   
-    // BONUS: Calculate summary statistics for the age field of the filtered data
-  
-    // First, create an array with just the age values
-    var ages = filteredData.map(person => person.age);
-  
-    // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-    var mean = math.mean(ages);
-    var median = math.median(ages);
-    var mode = math.mode(ages);
-    var variance = math.var(ages);
-    var standardDeviation = math.std(ages);
-  
-    // Then, select the unordered list element by class name
-    var list = d3.select(".summary");
-  
-    // remove any children from the list to
-    list.html("");
-  
-    // append stats to the list
-    list.append("li").text(`Mean: ${mean}`);
-    list.append("li").text(`Median: ${median}`);
-    list.append("li").text(`Mode: ${mode}`);
-    list.append("li").text(`Variance: ${variance}`);
-    list.append("li").text(`Standard Deviation: ${standardDeviation}`);*/
+    
   };
