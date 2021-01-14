b9244761888e94e887d50de3a83b74741c8bff2f
Emilia <e.lubanska@gmail.com>
2021-01-13 19:25:56 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,9 @@
-// from data.js
+// Read data from data.js
 var tableData = data;
-// Select the button
+
 var tbody = d3.select("tbody");
 
+// Select the button
 var button = d3.select("#button");
 
 // Select the form
@@ -10,33 +11,25 @@ var form = d3.select("#form");
 
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+form.on("submit", runEnter);
 
-data.forEach((UFOReport) => {
+data.forEach((UFO_Finder) => {
     var row = tbody.append("tr");
-    Object.entries(UFOReport).forEach(([value]) => {
+    Object.entries(UFO_Finder).forEach(([value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
 
 function runEnter() {
-
     // Prevent the page from refreshing
     d3.event.preventDefault();
-    
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("datetime");
-  
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
-  
     console.log(inputValue);
     console.log(tableData);
-  
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-  
     console.log(filteredData);
-  
-    
   };
